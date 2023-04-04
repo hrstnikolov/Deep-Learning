5a7cfc0e7c259b0cd43fd5a836537c85188dde5c
hrstnikolov <hrstnikolov@gmail.com>
2023-04-04 22:00:29 +0300

    updated nlp and exam prep

---
+++ jupyterlab.bat
@@ -0,0 +1,5 @@
+cd %homepath%
+call conda activate deep_learning
+call jupyter lab
+pause
+:: to automatically minimize the cmd window create shortcut to .bat and update the "Run" field in the shortcut file properties
